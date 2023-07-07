27766e9be03b4d0936bd218b73b63c46e9bea19d
Maik Emanoel <maikemanoel11@gmail.com>
2023-07-07 14:16:47 -0300

    add max screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,22 @@ export default {
       },
       dropShadow: {
         planetShadow: '0px 0px 70px rgba(112, 0, 255, 0.70)'
+      },
+      screens:{
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '768px'},
+        // => @media (max-width: 768px) { ... }
+  
+        'sm': {'max': '445px'},
+        // => @media (max-width: 445px) { ... }
       }
     },
   },
